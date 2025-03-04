3498a242ba1999f8c6b63d75b48965cb0775b526
076c <167858032+076c@users.noreply.github.com>
2025-03-04 20:06:14 +0100

    Add another print + learn to borrow inclusively.

--- main.rs
+++ main.rs
@@ -3,16 +3,27 @@ struct Person<'a> {
     age: u8
 }
 
-fn greet_person<'a>(person: Person<'a>) {
+fn greet_person<'a>(person: &Person<'a>) {
     println!("Hello {}!", &person.name);
 }
 
+#[allow(unused_parens)]
+fn what_is_age<'a>(person: &Person<'a>) -> u8 {
+    person.age
+}
+
+fn what_is_name<'a>(person: &Person<'a>) -> &'a str {
+    person.name
+}
+
 #[allow(dead_code)]
+#[allow(unused_mut)]
 fn main() {
     let mut person: Person = Person {
         name: "Charles",
         age: 30
     };
 
-    greet_person(person);
+    greet_person(&person);
+    println!("{} is {} years old.", what_is_name(&person), what_is_age(&person));
 }
